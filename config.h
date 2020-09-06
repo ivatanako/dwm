053c08eb38d2579bacda83c113f64e7b690543be
Juan Adrian Castro Quintana <juacq97@tutanota.com>
2020-09-05 21:50:42 -0600

    minor

--- config.h
+++ config.h
@@ -7,33 +7,33 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int rmaster            = 0;        /* 1 = master at right*/
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 24;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const int tag_padding        = 0;        
-static const int vertpad            = 4;       /* vertical padding of bar */
-static const int sidepad            = 8;       /* horizontal padding of bar */
-static const char *fonts[]          = {"Ubuntu:Regular:size=10", "Material Design Icons:Regular:pixelsize=16:antialias:true"};
+static const int user_bh            = 25;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int tag_padding        = 3;        
+static const int vertpad            = 0;       /* vertical padding of bar */
+static const int sidepad            = 0;       /* horizontal padding of bar */
+static const char *fonts[]          = {"Ubuntu:Regular:size=10.3", "Material Design Icons:Regular:pixelsize=16:antialias=true"};
 static const char dmenufont[]       = "monospace:size=10";
 
-static const char back[]   = "#282a36";
-static const char fore[]   = "#f8f8f2"; 
-static const char border[] = "#f8f8f2";
+static const char back[]   = "#282828";
+static const char fore[]   = "#fbf2c7"; 
+static const char border[] = "#fbf2c7";
+static const char col0[]   = "#1d2021";
+static const char col1[]   = "#cc241d";
+static const char col2[]   = "#98971a";
+static const char col3[]   = "#d79921";
+static const char col4[]   = "#458588";
+static const char col5[]   = "#b16286";
+static const char col6[]   = "#689d6a";
+static const char col7[]   = "#a89984";
+static const char col8[]   = "#928374";
+static const char col9[]   = "#fb4934";
+static const char col10[]  = "#b8bb26";
+static const char col11[]  = "#fabd2f";
+static const char col12[]  = "#83a598";
+static const char col13[]  = "#d3869b";
+static const char col14[]  = "#8ec07c";
+static const char col15[]  = "#ebdbb2";
 
-static const char col0[]   = "#000000";
-static const char col1[]   = "#FF5555";
-static const char col2[]   = "#50FA7B";
-static const char col3[]   = "#F1FA8C";
-static const char col4[]   = "#BD93F9";
-static const char col5[]   = "#FF79C6";
-static const char col6[]   = "#8BE9FD";
-static const char col7[]   = "#BFBFBF";
-static const char col8[]   = "#4D4D4D";
-static const char col9[]   = "#FF6E67";
-static const char col10[]  = "#5AF78E";
-static const char col11[]  = "#F4F99D";
-static const char col12[]  = "#CAA9FA";
-static const char col13[]  = "#FF92D0";
-static const char col14[]  = "#9AEDFE";
-static const char col15[]  = "#E6E6E6";
 static const char spotify[]= "#1FC167";
  
 
@@ -42,27 +42,27 @@ static const char *colors[][3]      = {
 	[SchemeNorm]     = { fore,      back,      col0   }, // \x0b
 	[SchemeSel]      = { fore,      back,      border }, // \x0c
 	[SchemeStatus]   = { fore,      back,      border }, // \x0d  Statusbar right 
-	[SchemeTagsSel]  = { col15,     col4,      border }, // \x0e  Tagbar left selected 
+	[SchemeTagsSel]  = { col4,      back,      border }, // \x0e  Tagbar left selected 
         [SchemeTagsNorm] = { fore,      back,      border }, // \x0f  Tagbar left unselected 
         [SchemeInfoSel]  = { fore,      back,      border }, // \x10  infobar middle  selected 
         [SchemeInfoNorm] = { fore,      back,      border }, // \x11  infobar middle  unselected 
-	[SchemeCol1]     = { back,      col1,      col0   }, // \x12 
-	[SchemeCol2]     = { back,      col2,      col0   }, // \x13
-	[SchemeCol3]     = { back,      col3,      col0   }, // \x14 
-	[SchemeCol4]     = { back,      col4,      col0   }, // \x15
-	[SchemeCol5]     = { back,      col5,      col0   }, // \x16 
-	[SchemeCol6]     = { back,      col6,      col0   }, // \x17
-	[SchemeCol7]     = { back,      col7,      col0   }, // \x18
-	[SchemeCol8]     = { back,      col8,      col0   }, // \x19
-	[SchemeCol9]     = { back,      col9,      col0   }, // \x1a
+	[SchemeCol1]     = { fore,      back,      col0   }, // \x12 
+	[SchemeCol2]     = { fore,      back,      col0   }, // \x13
+	[SchemeCol3]     = { fore,      back,      col0   }, // \x14 
+	[SchemeCol4]     = { fore,      back,      col0   }, // \x15
+	[SchemeCol5]     = { fore,      back,      col0   }, // \x16 
+	[SchemeCol6]     = { fore,      back,      col0   }, // \x17
+	[SchemeCol7]     = { fore,      back,      col0   }, // \x18
+	[SchemeCol8]     = { fore,      back,      col0   }, // \x19
+	[SchemeCol9]     = { fore,      back,      col0   }, // \x1a
 	[SchemeCol10]    = { back,      col10,      col0   }, // \x1b
 	[SchemeCol11]    = { back,      col11,      col0   }, // \x1c 
-	[SchemeCol12]    = { back,      spotify,   col0   }, // \x1d Spotify
+	[SchemeCol12]    = { fore,      back,   col0   }, // \x1d Spotify
 };
 
 /* tagging */
-static const char *tags[] = { "󰲌", "󰧮", "󰺷", "󰽴"};
-static const char *alttags[] = { "󰲋", "󰈙", "󰊗", "󰎇"};
+static const char *tags[] = { "󰄰", "󰄰", "󰄰", "󰄰"};
+static const char *alttags[] = { "󰄯", "󰄯", "󰄯", "󰄯"};
 
 static const Rule rules[] = {
 	/* xprop(1):
